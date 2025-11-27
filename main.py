0b179a2cad5ed96d0ced0c2ec57a4863601a025b
Ohaegbu Chukwubueze Victor <ohaegbuvictor76@gmail.com>
2025-11-27 11:21:23 +0100

    First working code with output image

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
-import numpy
-print("Anaconda is active")
+import numpy as np
+import matplotlib.pyplot as plt
+
+path = "input.jpeg"
+img_array = plt.imread(path)
+grayscale_weights = np.array([0.299, 0.857, 0.114])
+
+# Get the dot product of the weights and pixel values, round up elements to the nearest integer and covert elements to an unsigned 8 byte integer
+grayscale_image_arr = np.rint(np.dot(img_array,grayscale_weights)).astype("u8")
+
+plt.imshow(grayscale_image_arr, cmap="gray")
+plt.axis("off")
+plt.savefig("output.jpeg")
+plt.show()
