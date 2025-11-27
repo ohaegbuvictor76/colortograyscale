bb4f484e66d3b53d55656c42fb0828ea504fed89
Ohaegbu Chukwubueze Victor <ohaegbuvictor76@gmail.com>
2025-11-27 11:34:14 +0100

    Save the raw image  instead of the plot image

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ grayscale_weights = np.array([0.299, 0.587, 0.114])
 # Get the dot product of the weights and pixel values, round up elements to the nearest integer and covert elements to an unsigned 8 byte integer
 grayscale_image_arr = np.rint(np.dot(img_array,grayscale_weights)).astype("u8")
 
-# Covert the array to an Image
+# Covert the array to an image
 plt.imshow(grayscale_image_arr, cmap="gray")
 plt.axis("off") # Remove the axes
 
-# Save and show the image
-plt.savefig("output.jpeg")
+# Save the image in raw format and show
+plt.imsave("output.jpeg", grayscale_image_arr, cmap="gray")
 plt.show()
