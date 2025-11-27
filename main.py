315123ecfbe60bdb908c1a2020ec3281c26909e6
Ohaegbu Chukwubueze Victor <ohaegbuvictor76@gmail.com>
2025-11-27 11:24:38 +0100

    Correct the grayscale weights

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 path = "input.jpeg"
 img_array = plt.imread(path)
-grayscale_weights = np.array([0.299, 0.857, 0.114])
+grayscale_weights = np.array([0.299, 0.587, 0.114])
 
 # Get the dot product of the weights and pixel values, round up elements to the nearest integer and covert elements to an unsigned 8 byte integer
 grayscale_image_arr = np.rint(np.dot(img_array,grayscale_weights)).astype("u8")
